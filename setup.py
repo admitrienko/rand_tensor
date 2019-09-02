6efe3a9d9aa7bdf0162180bea7d2d3c80b8dc139
Anastasia Dmitrienko <34189027+admitrienko@users.noreply.github.com>
2019-09-02 18:07:14 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='rand_tensor',
-      version='0.1',
+      version='1.0.0',
       description='This code package generate random tensors with user-specified marginal means and covariances from one of two optional distributions. The first is the maximum-entropy-distribution with the specified marginal means and covariances. The second is the tensor-normal-distribution with the specified means and covariances.',
       license='General Public License',
       packages=['rand_tensor'],
